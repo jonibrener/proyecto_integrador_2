e578277a01db216a0788ce9b0dc0d7d4afbbaf4a
Jonathan <jbrener@udesa.edu,ar>
2020-05-14 05:38:10 -0300

    linkeado css, y creacion de buscador

--- proyecto_integrador_2/controllers/peliculasController.js
+++ proyecto_integrador_2/controllers/peliculasController.js
@@ -1,11 +1,15 @@
 let peliculasController = {
     home: function(req,res){
         var prueba = 'hola'
-        res.render('home', {prueba:prueba})
+        res.render('home', {prueba:prueba, pagina: "home"})
     },
     detalle: function(req,res){
         var prueba2 = req.params.id
-        res.render('detalle', {prueba2:prueba2})
+        res.render('detalle', {prueba2:prueba2, pagina:"detalle"})
+    },
+    buscador: function (req,res){
+        var prueba3 = req.params.nombre
+        res.render('buscador', {prueba3:prueba3, pagina: "busqueda"})
     }
 }
 
--- proyecto_integrador_2/routes/homeRouter.js
+++ proyecto_integrador_2/routes/homeRouter.js
@@ -4,7 +4,7 @@ var peliculasController = require('../controllers/peliculasController')
 
 router.get('/', peliculasController.home)
 router.get('/detalle/:id', peliculasController.detalle)
-
+router.get('/buscar/:nombre', peliculasController.buscador)
 
 
 module.exports = router;
