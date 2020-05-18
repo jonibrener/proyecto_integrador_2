0d00b6fa0626d847e550b6b7bc631ed2598cac06
Jonathan <jbrener@udesa.edu,ar>
2020-05-18 14:54:15 -0300

    soy re kpo

--- proyecto_integrador_2/routes/homeRouter.js
+++ proyecto_integrador_2/routes/homeRouter.js
@@ -4,7 +4,7 @@ var peliculasController = require('../controllers/peliculasController')
 
 router.get('/', peliculasController.home)
 router.get('/detalle', peliculasController.detalle)
-router.get('/buscar/?buscar:busqueda', peliculasController.buscador)
+router.get('/buscar', peliculasController.buscador)
 router.get ('/genero', peliculasController.genero)
 router.get('/estrenos', peliculasController.estrenos)
 router.get('/populares', peliculasController.populares)
