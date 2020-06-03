b5d839f21e9c4035464115bfde1bc6f3f5dca392
tobias <tvolberg@udesa.edu.ar>
2020-06-03 14:16:24 -0300

    Comenzando lo de alterar orden

---
+++ proyecto_integrador_2/public/javascript/mejoresPuntuadas.js
@@ -0,0 +1,9 @@
+let orden = 'ASC'
+document.getElementById('cambiarOrden').onclick=function(){
+    document.querySelectorAll(".divMejores").style.order = orden
+    
+    console.log("CLickeo");
+}
+
+
+
