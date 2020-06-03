ce11de66d580dffefdf19ef1ce6fa37966c55194
tobias <tvolberg@udesa.edu.ar>
2020-06-03 14:57:46 -0300

    Esperando respuesta de Dario

--- proyecto_integrador_2/public/javascript/mejoresPuntuadas.js
+++ proyecto_integrador_2/public/javascript/mejoresPuntuadas.js
@@ -1,6 +1,20 @@
 let orden = 'ASC'
 document.getElementById('cambiarOrden').onclick=function(){
-    document.querySelectorAll(".divMejores").style.order = orden
+    var divs = document.querySelectorAll(".divMejores")
+    if (orden == 'ASC') {
+        orden = 'DESC'
+        for (let index = 0; index < divs.length; index++) {
+            
+            
+        }
+    }else{
+        divs.style.order = 1
+        orden = 'ASC'
+        for (let index = 0; index < divs.length; index++) {
+            
+            
+        }
+    }
     
     console.log("CLickeo");
 }
